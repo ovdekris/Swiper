ce8986576b33447658d7c8f0ef8a0a4292414828
ovdekris <ovde.kris@gmail.com>
2023-03-01 12:46:29 +0100

    Create a structure of slider

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
-import {Swiper, SwiperSlide} from 'swiper/react';
-import { useSwiper } from 'swiper/react';
 import './App.css';
 import 'swiper/css';
 import "swiper/swiper-bundle.min.css";
 import Slider from "./Slider/Slider";
-import Title from "./Slider/Title/Title";
-const name=[
-    {title:"This is React slider"},
-    {title:"This is React slider"},
-    {title:"This is React slider"}
-]
+
+
+
 function App() {
   return (
     <div className="App">
-        <Title props={name}/>
+        <h1>Carousel</h1>
       <Slider/>
     </div>
   );
--- src/Slider/Slider.js
+++ src/Slider/Slider.js
@@ -1,26 +1,47 @@
 import {Swiper, SwiperSlide} from 'swiper/react';
 import 'swiper/css';
 import "swiper/swiper-bundle.min.css"
-import {SwiperButtonNext} from "./ButtonNext/SwiperButtonNext";
-import {SwiperButtonPrev} from "./ButtonPrev/SwiperButtonPrev";
-import { EffectFade } from 'swiper';
-import img1 from "../image1.jpeg";
-import img2 from "../image2.png";
-import {Navigation} from "swiper";
+import {Navigation, Pagination} from "swiper";
 import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import style from "./Slide.module.css";
+import img from "./img1.jpg"
+
 export default function Slider(props){
-    console.log(props.props[0])
+
     return(
     <Swiper
         loop={true}
         slidesPerView={1}
-        modules={[Navigation]}
+        modules={[Navigation,Pagination]}
         navigation={true}
+        pagination={{ clickable: true }}
         spaceBetween={10}
+        className={style.container}
 
     >
-        {/*<p>{`"${props.props[0]}"`}</p>*/}
-        {/*<SwiperSlide ><img src={props.props[0]} alt=""/></SwiperSlide>*/}
+
+        <SwiperSlide className={style.slider}>
+            <div className={style.slider__container}>
+                <div className={style.slider__container__info}>
+      fff
+                </div>
+                <div className={style.slider__container__image}><img src={img} alt="" className={style.image}/></div>
+            </div>
+        </SwiperSlide>
+        <SwiperSlide className={style.slider}>
+            <div className={style.slider__container}>
+                <div className={style.slider__container__info}></div>
+                <div className={style.slider__container__image}></div>
+            </div>
+        </SwiperSlide>
+        <SwiperSlide className={style.slider}>
+            <div className={style.slider__container}>
+                <div className={style.slider__container__info}></div>
+                <div className={style.slider__container__image}></div>
+            </div>
+        </SwiperSlide>
+
 
     </Swiper>)
 
